3e245bf0bc7ba32567c661fff4026d33e024896c
M_Usama_I <usama@erpfy.co>
2020-04-28 18:10:39 +0500

    Update custom_time_sheet.py

--- custom_timesheet_erpify/model/custom_time_sheet.py
+++ custom_timesheet_erpify/model/custom_time_sheet.py
@@ -25,7 +25,7 @@ class Timesheets(models.Model):
 
     @api.model
     def create(self, vals):
-        employee = self.env.user.employee_id.id
+        employee = self.env.user.employee_id
         vals.update({'project_id': employee.project_id_erpify.id,
                      'account_id': employee.project_id_erpify.analytic_account_id.id})
         result = super(Timesheets, self).create(vals)
