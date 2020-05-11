ef226ec74170201600ae26a6186c6f93bdd18da4
M_Usama_I <usama@erpfy.co>
2020-05-11 17:00:27 +0500

    Update: Contract History

--- eir_employee_customizations/models/hr_employee.py
+++ eir_employee_customizations/models/hr_employee.py
@@ -6,4 +6,8 @@ class Contract(models.Model):
     _inherit = 'hr.contract'
 
     wage = fields.Monetary(string='Total Salary')
-
+    basic_db = fields.Monetary(related='x_basic_db')
+    basic_non_db = fields.Monetary(related='x_basic_non_db')
+    grade = fields.Selection(related='x_grade')
+    scale_point = fields.Char(related='x_scale_point')
+    pay_group = fields.Selection(related='x_pay_group')
