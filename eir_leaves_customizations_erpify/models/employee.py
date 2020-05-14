1a5843ddb1b6e08647ade148d1bfdbf9c7909ab7
M_Usama_I <usama@erpfy.co>
2020-05-14 14:33:44 +0500

    Update employee.py

--- eir_leaves_customizations_erpify/models/employee.py
+++ eir_leaves_customizations_erpify/models/employee.py
@@ -3,14 +3,14 @@ from odoo.exceptions import ValidationError, Warning
 from odoo.tools.safe_eval import safe_eval
 from datetime import date, timedelta
 import time
-import dateutil
+from dateutil.relativedelta import relativedelta
 
 
 class Employee(models.Model):
     _inherit = "hr.employee"
 
     def get_number_of_leaves_in(self, leave_types=[], lastmonths=12):
-        since = date.today() - timedelta(months=lastmonths)
+        since = date.today() - relativedelta(months=lastmonths)
         applied_leaves = self.env['hr.leave'].search([('employee_id', '=', self.id), ('state', '=', 'validate'), ('holiday_status_id', 'in', leave_types),
                                      ('request_date_from', '>=', since)])
         if applied_leaves:
